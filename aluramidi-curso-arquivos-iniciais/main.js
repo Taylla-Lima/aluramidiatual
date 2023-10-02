5df8642a7b1a5d63a8d428727a0d4628c93ef6cc
Taylla-Lima <119326235+Taylla-Lima@users.noreply.github.com>
2023-10-02 12:27:17 -0300

    Add files via upload

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -9,10 +9,19 @@ let contador = 0;
 
 while (contador < listaDeTeclas.length) {
 
-    listaDeTeclas[contador].onclick = tocaSom;   
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    //console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
 
     contador = contador + 1;
 
-    console.log(contador);
+    //console.log(contador);
 
-}
+} 

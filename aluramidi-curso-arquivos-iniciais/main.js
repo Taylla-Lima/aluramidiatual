9b7cf187d3605620d087607080c14d1aaebfcfe7
Taylla-Lima <119326235+Taylla-Lima@users.noreply.github.com>
2023-10-16 12:26:23 -0300

    Add files via upload

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -2,8 +2,15 @@ function tocaSom (selectorAudio) {
    const elemento = document.querySelector(selectorAudio);
 
    if (elemento === null) { 
-       alert('Elemento não encontrado');
+       //alert('Elemento não encontrado');
+       console.log('Elemento não encontrado');
    }
+
+   if (elemento != null) {
+    if(elemento.localName === 'audio') {
+       elemento.play();
+   }
+}
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
